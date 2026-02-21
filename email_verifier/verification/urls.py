497593d2d0e78042e028f83b776b8fc4bf27a493
RohitMane1208 <x24228125@student.ncirl.ie>
2026-02-21 11:37:10 +0000

    Delete email_verifier directory

--- email_verifier/verification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views #update1
-
-urlpatterns = [
-    path('', views.verify_email, name='verify_email'),
-    path('verify/<uuid:token>/', views.verify_token, name='verify_token'),
-]
