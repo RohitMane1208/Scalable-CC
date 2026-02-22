f4b0375185613e14ebc26221d07fcad7e50d1cac
EC2 Default User <ec2-user@ip-172-31-14-131.ec2.internal>
2026-02-22 11:41:22 +0000

    API status2

--- EmailVerificationProject/email_verifier/verification/urls.py
+++ EmailVerificationProject/email_verifier/verification/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views # checking
 
 urlpatterns = [
-    path('', views.verify_email, name='verify_email'),
     path('verify/<uuid:token>/', views.verify_token, name='verify_token'),
     path('api/status/', views.check_verification_status, name='check_status'),
+    path('', views.verify_email, name='verify_email'),
 ]
